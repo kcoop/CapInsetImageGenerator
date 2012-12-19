15f3d897a1273f729d1268212d2417e58131a35f
Ken Cooper <ken@coopercode.net>
2012-12-19 09:34:20 -0800

    Initial Commit

---
+++ CapInsetImageGenerator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CapInsetImageGenerator
+//
+//  Created by Ken Cooper on 12/19/12.
+//  Copyright (c) 2012 Coopercode. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
